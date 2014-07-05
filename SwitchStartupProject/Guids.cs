d6d022bcff598fad72a641278b2ea33c1f7e2a07
andreas <andreas@localhost>
2014-07-05 18:41:00 +0200

    Add support for Website projects * Web site projects must be set with their full path (fix #12)

--- SwitchStartupProject/Guids.cs
+++ SwitchStartupProject/Guids.cs
@@ -13,6 +13,7 @@ namespace LucidConcepts.SwitchStartupProject
 
         public static readonly Guid guidCPlusPlus = new Guid("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}");
         public static readonly Guid guidWebApp = new Guid("{349c5851-65df-11da-9384-00065b846f21}");
+        public static readonly Guid guidWebSite = new Guid("{e24c65dc-7377-472b-9aba-bc803b73c61a}");
         public static readonly Guid guidVsPackage = new Guid("{82b43b9b-a64c-4715-b499-d71e9ca2bd60}");
         public static readonly Guid guidDatabase = new Guid("{00d1a9c2-b5f0-4af3-8072-f6c62b433612}");
     };

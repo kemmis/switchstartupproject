cf70873924dd5a421bd815bef78ef5243ee640c4
andreas <andreas@localhost>
2016-11-26 17:20:48 +0100

    Change package GUID and name for separate extension (see #47)

--- SwitchStartupProject/Guids.cs
+++ SwitchStartupProject/Guids.cs
@@ -6,7 +6,7 @@ namespace LucidConcepts.SwitchStartupProject
 {
     static class GuidList
     {
-        public const string guidSwitchStartupProjectPkgString = "894b6873-138b-4e5f-ac68-6863cf312f7b";
+        public const string guidSwitchStartupProjectPkgString = "40b5ca05-6793-42f7-a50a-cca5bf4ff495";
         public const string guidSwitchStartupProjectCmdSetString = "9c1b4719-1443-4d96-b264-7cf17809659b";
 
         public static readonly Guid guidSwitchStartupProjectCmdSet = new Guid(guidSwitchStartupProjectCmdSetString);

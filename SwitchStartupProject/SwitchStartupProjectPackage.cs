1c067f127b25e2d140a14702657965061bd8f37d
Chris Bordeman <cbordeman@outlook.com>
2024-03-03 14:54:07 -0500

    Created new project and solution file, restructured and fixed a few things to make compile.

--- SwitchStartupProject/SwitchStartupProjectPackage.cs
+++ SwitchStartupProject/SwitchStartupProjectPackage.cs
@@ -1,13 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Globalization;
-using System.Linq;
-using System.Runtime.InteropServices;
-
-using Microsoft.VisualStudio;
+﻿using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SwitchStartupProject
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    /// 
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExistsAndFullyLoaded_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [Guid(SwitchStartupProjectPackage.PackageGuidString)]
+    public sealed class SwitchStartupProjectPackage : AsyncPackage
+    {
+        /// <summary>
+        /// SwitchStartupProjectPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "ec97f39c-4d5f-4bda-8bac-5d778ea1b6d5";
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+
+        #endregion
+    }
+}
 
 namespace LucidConcepts.SwitchStartupProject
 {
@@ -66,10 +115,10 @@ namespace LucidConcepts.SwitchStartupProject
             base.Initialize();
 
             // Add our command handlers for menu (commands must exist in the .vsct file)
-            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            if ( null == mcs )
+            var mcs = (IMenuCommandService)GetService(typeof(IMenuCommandService));
+            if (null == mcs)
             {
-                Logger.LogInfo("Could not get OleMenuCommandService");
+                Logger.LogInfo("Could not get IMenuCommandService");
                 return;
             }
             var dropdownService = new DropdownService(mcs);
@@ -286,7 +335,7 @@ namespace LucidConcepts.SwitchStartupProject
 
             private void Log(__ACTIVITYLOG_ENTRYTYPE type, string message, params object[] arguments)
             {
-                var log = package.GetService(typeof (SVsActivityLog)) as IVsActivityLog;
+                var log = package.GetService(typeof(SVsActivityLog)) as IVsActivityLog;
                 if (log == null) return;
                 log.LogEntry((UInt32)type, "SwitchStartupProject", string.Format(CultureInfo.CurrentCulture, message, arguments));
             }
